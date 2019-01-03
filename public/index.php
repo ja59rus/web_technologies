a1c61aca098aec7dd079da0590d42b64bc036939
Андрей Мурашов <mur101@yandex.ru>
2019-01-03 21:08:59 +0500

    index.php забыл прикрепить

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../includes/db_connection.php');
+require_once('../includes/function.php');
+?>
+
+<?php require_once('../includes/layouts/header.php'); ?>
+
+<div id="main">
+
+</div>
+<?php require_once('../includes/layouts/footer.php'); ?>
